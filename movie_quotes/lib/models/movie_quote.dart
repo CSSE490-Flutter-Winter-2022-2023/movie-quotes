539c2b46ccc393f8547bf3dbf9279c7d9a48a687
David Fisher <fisherds@rose-hulman.edu>
2023-01-13 10:19:53 -0500

    ran firebase config

--- movie_quotes/lib/models/movie_quote.dart
+++ movie_quotes/lib/models/movie_quote.dart
@@ -1,6 +1,14 @@
+// Movie Quotes
+const kMovieQuotesCollectionPath = "MovieQuotes";
+const kMovieQuoteQuote = "quote";
+const kMovieQuoteMovie = "movie";
+const kMovieQuoteLastTouched = "lastTouched";
+
 class MovieQuote {
+  String? documentId;
   String quote;
   String movie;
+  // Timestamp created;
 
   MovieQuote({required this.quote, required this.movie});
 }
