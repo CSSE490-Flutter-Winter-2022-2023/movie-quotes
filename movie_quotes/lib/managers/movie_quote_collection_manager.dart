f762de0cd51620b69af19bf5fe9070d4fe3102fc
David Fisher <fisherds@rose-hulman.edu>
2023-01-17 14:14:51 -0500

    add

--- movie_quotes/lib/managers/movie_quote_collection_manager.dart
+++ movie_quotes/lib/managers/movie_quote_collection_manager.dart
@@ -18,9 +18,32 @@ class MovieQuotesCollectionManager {
         .orderBy(kMovieQuote_lastTouched, descending: true)
         .snapshots()
         .listen((QuerySnapshot querySnapshot) {
+      latestMovieQuotes =
+          querySnapshot.docs.map((doc) => MovieQuote.from(doc)).toList();
+      observer();
+
       print(querySnapshot.docs);
     });
   }
 
-  // TODO: Make a stop listening
+  void stopListening(StreamSubscription? subscription) {
+    subscription?.cancel();
+  }
+
+  Future<String> add({
+    required String quote,
+    required String movie,
+  }) {
+    return _ref
+        .add({
+          kMovieQuote_quote: quote,
+          kMovieQuote_movie: movie,
+          kMovieQuote_lastTouched: Timestamp.now(),
+        })
+        .then((docRef) => docRef.id)
+        .catchError((error) {
+          print("Failed to add registration: $error");
+          return "";
+        });
+  }
 }
