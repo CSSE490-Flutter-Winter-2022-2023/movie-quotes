a239df1f5cbad31830b8d1b4c682bc3d751e4f02
David Fisher <fisherds@rose-hulman.edu>
2023-01-24 13:25:13 -0500

    progress on DIY email password UI

--- movie_quotes/lib/pages/email_password_auth_page.dart
+++ movie_quotes/lib/pages/email_password_auth_page.dart
@@ -14,14 +14,79 @@ class EmailPasswordAuthPage extends StatefulWidget {
 }
 
 class _EmailPasswordAuthPageState extends State<EmailPasswordAuthPage> {
+  final emailTextController = TextEditingController();
+  final passwordTextController = TextEditingController();
+
+  @override
+  void dispose() {
+    emailTextController.dispose();
+    passwordTextController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Text(
-            widget.isNewUser ? "Create a user" : "Log in an existing user"),
-      ),
-    );
+        appBar: AppBar(
+          title: Text(widget.isNewUser
+              ? "Create a New User"
+              : "Log in an Existing User"),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 250.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  controller: emailTextController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Email',
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  controller: passwordTextController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Password',
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 15.0,
+              ),
+              Container(
+                height: 40.0,
+                width: 220.0,
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                child: TextButton(
+                  onPressed: () {
+                    print(
+                        "Email: ${emailTextController.text}  Password: ${passwordTextController.text}");
+                    if (widget.isNewUser) {
+                      print("TODO: Create a new user!");
+                    } else {
+                      print("TODO: Log in an existing user");
+                    }
+                  },
+                  child: Text(
+                    widget.isNewUser ? "Create Account" : "Log In",
+                    style: TextStyle(color: Colors.white, fontSize: 18.0),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
